59beaf8279ddffefcb34e64679c9dc87491e7f5e
Ibrahim Jarada <ibrahim.jarada@gmail.com>
2022-01-20 00:43:47 +0200

    home page

---
+++ main.js
@@ -0,0 +1,9 @@
+const input=document.querySelector('input');
+input.value=localStorage.getItem('name');
+input.focus()
+document.querySelector('button').addEventListener('click',e=>{
+    if(input.value){
+        localStorage.setItem('name',input.value);
+        location.href="./game-start/game-start.html"
+    }
+})
